707c088cd54a7e459f68184c9ac9d4ee4959da32
devseop <cocc263@gmail.com>
2023-11-21 19:37:59 +0900

    💄 Feat: Header 구현 1. Drawer Button(햄버거 메뉴 아이콘) 클릭시 사이드 메뉴 노출 2. 로고 클릭시 메인페이지로 이동 3. 비로그인시: “로그인 / 회원가입” 문구 노출, 클릭시 회원가입 / 로그인 페이지로 이동

---
+++ pages/components/Layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
+}
+
+export default Layout;
