e1c477c36ac3f6bb1ac8da9dc33ceb4f0f7eb6f2
devseop <cocc263@gmail.com>
2023-11-24 00:44:41 +0900

    :wrench: Fix: `next/image` Un-configured Host 오류 수정
    현재 넥스트 버전에 맞는 방식으로 config 수정

--- next.config.js
+++ next.config.js
@@ -3,7 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['images.unsplash.com'],
+    // domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/photo**',
+      },
+    ],
   },
 };
 
